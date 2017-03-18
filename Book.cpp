ed585636b3f8e006d3a7812b1e13eecc76464ccb
Sajal Narang <sajalnarang@gmail.com>
2017-03-18 19:23:00 +0530

    Add Constructors, Getters and Setters for class Book

--- Book.cpp
+++ Book.cpp
@@ -1,13 +1,74 @@
 #include"Book.h"
 
 class Book{
-  String title;
-  String author;
-  long int book_id;
+	String title;
+	String author;
+	long unsigned int book_id;
 
 public:
-  void add_book(); //allocates memory to new book
-  void delete_book();
-  
+	/**
+	 * Default constructor
+	 */
+	Book() {
+		this->title = "";
+		this->author = "";
+		this->book_id = 0;
+	}
 
+	/**
+	 * Parameterized constructor
+	 * @param title The title of the book
+	 * @param author The name of the author of the book
+	 * @param book_id The unique ID given to each book in the library
+	 */
+	Book(String title, String author, int book_id) {
+		this->title = title;
+		this->author = author;
+		this->book_id = book_id;
+	}
+
+	/**
+	 * Returns the title of the book
+	 */
+	String getTitle() {
+		return title;
+	}
+
+	/**
+	 * Changes the title of the book
+	 * @param title The new title that is to be set
+	 */
+	void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * Returns the author of the book
+	 */
+	String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * Changes the author of the book
+	 * @param title The new author name that is to be set
+	 */
+	void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/**
+	 * Returns the ID of the book
+	 */
+	long unsigned int getBook_id() {
+		return book_id;
+	}
+
+	/**
+	 * Changes the ID of the book
+	 * @param title The new ID that is to be set
+	 */
+	void setBook_id(int book_id) {
+		this.book_id = book_id;
+	}
 };
