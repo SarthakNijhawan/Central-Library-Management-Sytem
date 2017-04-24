51a26af02a988a8c868e41602e7a695b7b70f246
Sajal Narang <sajalnarang@gmail.com>
2017-04-25 02:55:04 +0530

    Init claim and issue classes

---
+++ Issue.cpp
@@ -0,0 +1,74 @@
+#include "Issue.h"
+
+class Issue {
+    Date issue_date;
+    Date due_date;
+    float fine;
+    Book book;
+    User user;
+
+public:
+    Issue() {
+        this->issue_date = NULL;
+        this->due_date = NULL;
+        this->fine = 0;
+        this->book = NULL;
+        this->user = NULL;
+    }
+
+    Issue(Date issue_date, Date due_date, float fine, Book book, User user) {
+        this->issue_date = issue_date;
+        this->due_date = due_date;
+        this->fine = fine;
+        this->book = book;
+        this->user = user;
+    }
+
+    Issue(const Issue &original) {
+        this->issue_date = original.issue_date;
+        this->due_date = original.due_date;
+        this->fine = original.fine;
+        this->book = original.book;
+        this->user = original.user;
+    }
+
+    Date getIssueDate() {
+        return issue_date;
+    }
+
+    void setIssueDate(Date issue_date) {
+        this->issue_date = issue_date;
+    }
+
+    Date getDueDate() {
+        return due_date;
+    }
+
+    void setDueDate(Date due_date) {
+        this->due_date = due_date;
+    }
+
+    float getFine() {
+        return fine;
+    }
+
+    void setFine(float fine) {
+        this->fine = fine;
+    }
+
+    Book getBook() {
+        return book;
+    }
+
+    void setBook(Book book) {
+        this->book = book;
+    }
+
+    User getUser() {
+        return user;
+    }
+
+    void setUser(User user) {
+        this->user = user;
+    }
+}
